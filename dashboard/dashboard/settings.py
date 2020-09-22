2d4b79fe2cf1a9c5527bc214db61632631ba8b5a
Jay Prakash <50571500+jayprakash02@users.noreply.github.com>
2020-09-22 20:28:01 +0530

    allauth succesfully set

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -121,7 +122,11 @@ USE_L10N = True
 USE_TZ = True
 
 SITE_ID = 1
+LOGIN_REDIRECT_URL = 'dashboard/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
